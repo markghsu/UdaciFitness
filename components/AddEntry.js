4ab51ae6d334c1d96e5ed1539037a882d1b5e153
Mark Hsu <mark.g.hsu@gmail.com>
2017-11-09 23:46:31 -0800

    add submit button

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
-import {getMetricMetaInfo} from '../utils/helpers'
+import {View, TouchableOpacity, Text} from 'react-native'
+import {getMetricMetaInfo, timeToString} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+
+function SubmitBtn ({onPress}) {
+	return (
+		<TouchableOpacity onPress={onPress}>
+			<Text>Submit</Text>
+		</TouchableOpacity>
+	)
+}
+
 
 export default class addEntry extends Component {
 	state = {
@@ -36,6 +45,25 @@ export default class addEntry extends Component {
 	slide = (metric,value) => {
 		this.setState(()=>({[metric]:value}))
 	}
+	submit = () => {
+		const key = timeToString()
+		const entry = this.state
+
+		this.setState(()=> ({
+			run:0,
+			bike:0,
+			swim:0,
+			sleep:0,
+			eat:0
+		}))
+		//Update Redux
+
+		//Navigate to Home
+
+		//Save to DB
+
+		//Clear local notification
+	}
 	render(){
 		const metaInfo = getMetricMetaInfo()
 		return (
@@ -65,6 +93,8 @@ export default class addEntry extends Component {
 						</View>
 					)
 				})}
+
+					<SubmitBtn onPress={this.submit}/>
 			</View>
 		)
 	}
