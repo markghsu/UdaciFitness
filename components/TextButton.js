72650679e00e91445ac25292cfda1a0f8d7d4d52
Mark Hsu <mark.g.hsu@gmail.com>
2017-11-15 22:39:31 -0800

    Style text Button

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react'
-import {View, Text, TouchableOpacity} from 'react-native'
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native'
+import {white,orange,blue,purple,lightPurp,gray,red,pink} from '../utils/colors.js'
 
 
-export default function TextButton ({children,onPress}) {
+export default function TextButton ({children,onPress,style={}}) {
 	return (
 		<View>
 			<TouchableOpacity onPress={onPress}>
 				<View>
-					<Text>{children}</Text>
+					<Text style={[styles.reset, style]}>{children}</Text>
 				</View>
 			</TouchableOpacity>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	reset:{
+		textAlign: 'center',
+		color: purple
+	}
+})
