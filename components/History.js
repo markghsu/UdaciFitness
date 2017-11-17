8edf51b0399cfbf2a9f2862052eab25a7940fbff
Mark Hsu <mark.g.hsu@gmail.com>
2017-11-16 21:53:37 -0800

    Add metric card with styling

--- components/History.js
+++ components/History.js
@@ -7,6 +7,7 @@ import {fetchCalendarResults} from '../utils/api'
 import UdaciFitnessCalendar from 'udacifitness-calendar'
 import {white,purple} from '../utils/colors'
 import DateHeader from './DateHeader'
+import MetricCard from './MetricCard'
 
 class History extends Component {
 	componentDidMount(){
@@ -29,7 +30,7 @@ class History extends Component {
 					</Text>
 				</View>
 			:<TouchableOpacity onPress={()=>{console.log('pressed')}}>
-				<Text>{JSON.stringify(metrics)}</Text>
+				<MetricCard metrics={metrics} date={formattedDate} />
 			</TouchableOpacity>}
 		</View>
 	)
